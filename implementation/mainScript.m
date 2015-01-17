57898da42b5dc96e2700a0ca0d0b4b1d413cad40
KaffeLatte <aalto.erik@gmail.com>
2015-01-17 11:47:55 +0100

    Added selection step to mainscript

--- implementation/mainScript.m
+++ implementation/mainScript.m
@@ -27,24 +27,41 @@ for run=1:number_of_runs
     P = P_init * ones(T, N);
     x_predict = ones(T, N);
     P_predict = ones(T, N);
-    x_mean = ones(T, 1);
+    x_mean_predict = ones(T, 1);
     y_predict = ones(T, N);
     weights = ones(T, N) ./ N;
     
     for t = 2:T
-    % Prediction step
-    for i=1:N
-        [x_mean(t,i), P_predict(t,i)] = ...
-            ukf(x(t-1,i), P(t-1,i), [], Q, 'ukf_ffun', yt(t), R, 'ukf_hfun', t, alpha, beta, kappa);
-        %[x_mean(t,i), P_predict(t,i)] = ...
-        %    UKF(x(t-1,i), P(t-1,i), [], Q, yt(t), R, t, alpha, beta, kappa);
-        x_predict(t, i) = x_mean(t, i) + sqrtm(P_predict(t, i)) * randn(1, 1);
-        %x_predict(t,i) = normrnd(x_mean(t,i), sqrtm(P_predict(t,i)), 1, 1)
-    end
-    
-    % Evaluate importance weights
-
-    % Selection step
+        % Prediction step
+        for i=1:N
+            [x_mean_predict(t,i), P_predict(t,i)] = ...
+                UKF(x(t-1,i), P(t-1,i), [], Q, yt(t), R, t, alpha, beta, kappa);
+            %[x_mean(t,i), P_predict(t,i)] = ...
+            %    ukf(x(t-1,i), P(t-1,i), [], Q, 'ukf_ffun', yt(t), R, 'ukf_hfun', t, alpha, beta, kappa);
+            
+            % Sample x-hat
+            %------------------------------
+            % TODO: Try to generate x_predict with P_predict directly, without
+            % the use of sqrtm().
+            x_predict(t,i) = normrnd(x_mean_predict(t,i), sqrtm(P_predict(t,i)), 1, 1)
+            %x_predict(t, i) = x_mean(t, i) + sqrtm(P_predict(t, i)) * randn(1, 1);
+            
+        end
+        
+        % Evaluate importance weights up to a normalizing constant
+        for i=1:N,
+            
+            
+            
+        end
+        
+        
+        % Selection step
+        resampledPoints = randsample(N,N,true,w(t,:));
+        x(t, :) = x(t, resampledPoints);
+        P(t, :) = P(t, resampledPoints);
+        
+        
     end
 end    
 % Plot generated data
