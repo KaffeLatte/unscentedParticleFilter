ef50ffabcc24d5d7eeebb215ee64da2452b81aee
Kelly Karipidou <kellyk@kth.se>
2015-01-17 11:46:57 +0100

    Updated UKF function

--- implementation/UKF.m
+++ implementation/UKF.m
@@ -1,13 +1,18 @@
-function [x_mean_predict, P_predict] = UKF(x_prev, P_prev )
+function [x_mean_est, P_est] = UKF(x_prev, P_prev, U, Q, y_true, R, t, alpha, beta, kappa  )
 % Input:
 % Output:
 % x_mean_predict
+% P_predict
+
+%UKF(x(t-1,i), P(t-1,i), [], Q, yt(t), R, t, alpha, beta, kappa);
 
 
-%[x_mean(t,i), P_predict(t,i)] = ...
-        %    UKF(x(t-1,i), P(t-1,i), [], Q, yt(t), R, t, alpha, beta, kappa);
 
+K = Pxy / P_yy;
 
+x_mean_est = x_mean_pred + K* y - y_mean_pred;
+
+P_est = P_pred - K*P_yy*K';
 
 end
 
